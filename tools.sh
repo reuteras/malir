373af1f63af8d114a60bda94af1b9cb1739e4482
Peter Reuterås <peter@reuteras.net>
2022-09-23 07:48:12 +0200

    Install more useful packages.

--- tools.sh
+++ tools.sh
@@ -2,6 +2,33 @@
 #
 
 sudo apt install -y -q \
+	ascii \
+	bless \
+	bsdgames \
+	ctags \
+	dos2unix \
+	exfat-fuse \
+	exfat-utils \
 	git \
+	jq \
+	ngrep \
+	p7zip \
+	python3-yara \
+	screen \
+	shareutils \
+	sqlite3 \
+	sqlitebrowser \
+	strace \
+	tcpslice \
+	tmux \
+	tshark \
+	unrar \
 	vim \
-	wireshark
+	vim-doc \
+	vim-scripts \
+	whois \
+	wireshark \
+	wswedish \
+	yara \
+	yara-doc \
+	zip
