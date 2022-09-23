e3ae1e265f70a7f9068b54575909d5667cb16f7b
Peter Reuterås <peter@reuteras.net>
2022-09-23 13:59:48 +0200

    Add clean.sh and zero.sh.

--- clean.sh
+++ clean.sh
@@ -3,8 +3,4 @@
 sudo apt-get -y autoremove
 sudo apt-get autoclean
 sudo apt-get clean
-
-dd if=/dev/zero of="$HOME/zero" conv=fsync
-sleep 1
-sync
-rm -f "$HOME"/zero
+sudo docker system prune --force
--- zero.sh
+++ zero.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sudo apt-get -y autoremove
-sudo apt-get autoclean
-sudo apt-get clean
-
 dd if=/dev/zero of="$HOME/zero" conv=fsync
 sleep 1
 sync
