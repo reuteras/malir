04e94a11ac21f2f9f85a2478f05c6ea9298cc70a
Peter Reuterås <peter@reuteras.com>
2023-09-18 12:27:20 +0200

    Should echo n

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ function install-google-chrome() {
 function malcolm-configure() {
     info-message "Starting interactive configuration of Malcolm"
     cd ~/Malcolm || exit
-    echo "y" | sudo python3 scripts/install.py --defaults \
+    echo "n" | sudo python3 scripts/install.py --defaults \
         --dark-mode true \
         --suricata-rule-update true \
 		--extracted-file-server true \
