63e325cff092828bc63db0ee555b85502821b1c1
Peter Reuterås <peter@reuteras.com>
2023-09-19 14:03:01 +0200

    Fix script

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function update-zeek-intel-critical-path-security(){
-    cd ~/Malcolm/zeek/intel || exit
+    cd ~/Malcolm/zeek/intel/Zeek-Intelligence-Feeds || exit
     git fetch origin master
     git reset --hard FETCH_HEAD
     git clean -df 
