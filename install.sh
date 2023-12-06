e6a72a787ba0c0cb78ff418ad371e7fa7c2bf14a
Peter Reuterås <peter@reuteras.com>
2023-12-06 09:33:52 +0100

    Start testing new version v23.12.0

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 CONFIG_DIR="${HOME}/.config/manir"
 PATH="${PATH}:/usr/libexec/docker/cli-plugins"
-MALCOLM_VERSION="v23.09.0"
+MALCOLM_VERSION="v23.12.0"
 export PATH
 export DEBIAN_FRONTEND=noninteractive
 
