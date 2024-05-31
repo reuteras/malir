a368277995249857d7a41c28db2c2d7dd91e2c11
Peter Reuterås <peter@reuteras.com>
2024-05-31 10:46:39 +0200

    Silent install and add info-message

--- install.sh
+++ install.sh
@@ -53,17 +53,19 @@ function install-docker(){
     if dpkg --list | grep docker > /dev/null ; then
         touch "${CONFIG_DIR}/os_done"
     fi
-    sudo apt-get install ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    info-message "Make sure curl and ca-certificates are installed."
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -y install ca-certificates curl > /dev/null 2>&1
+    sudo install -m 0755 -d /etc/apt/keyrings > /dev/null 2>&1
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc > /dev/null 2>&1
     sudo chmod a+r /etc/apt/keyrings/docker.asc
     # shellcheck disable=SC1091
     echo \
           "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
             $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
               sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get update > /dev/null 2>&1
+    info-message "Install Docker."
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null 2>&1
     touch "${CONFIG_DIR}/docker_done"
 }
 
