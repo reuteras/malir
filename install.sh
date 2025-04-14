afcd65510b527c10f550583805cea61186a7dfeb
Peter Reuterås <peter@reuteras.net>
2025-04-14 15:01:08 +0200

    Update version uses for Malcolm.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 CONFIG_DIR="${HOME}/.config/manir"
 PATH="${PATH}:/usr/libexec/docker/cli-plugins"
-MALCOLM_VERSION="v25.01.0"
+MALCOLM_VERSION="v25.03.1"
 export PATH
 export DEBIAN_FRONTEND=noninteractive
 
