4b3b5f93f7fd5614995706a1965e71c0c2916fc8
Peter Reuterås <peter@reuteras.com>
2025-11-04 13:33:23 +0100

    Upgrade to version v25.11.0

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 CONFIG_DIR="${HOME}/.config/manir"
 PATH="${PATH}:/usr/libexec/docker/cli-plugins"
-MALCOLM_VERSION="v25.09.0"
+MALCOLM_VERSION="v25.11.0"
 export PATH
 export DEBIAN_FRONTEND=noninteractive
 
