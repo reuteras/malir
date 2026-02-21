3b12b8e2cd0c32e42a7b3fae2fb6da5d5a5551fe
Peter Reuterås <peter@reuteras.com>
2026-02-21 09:46:43 +0100

    Update version

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 CONFIG_DIR="${HOME}/.config/malir"
 PATH="${PATH}:/usr/libexec/docker/cli-plugins"
-MALCOLM_VERSION="v26.01.0"
+MALCOLM_VERSION="v26.02.0"
 export PATH
 export DEBIAN_FRONTEND=noninteractive
 
