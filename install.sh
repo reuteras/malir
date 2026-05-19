2355cbc2d613beb304a094985c1e398147546585
Peter Reuterås <peter@reuteras.net>
2026-05-19 04:04:38 +0200

    Update to latest version with nginx security fixes

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 CONFIG_DIR="${HOME}/.config/malir"
 PATH="${PATH}:/usr/libexec/docker/cli-plugins"
-MALCOLM_VERSION="v26.05.0"
+MALCOLM_VERSION="v26.05.1"
 export PATH
 export DEBIAN_FRONTEND=noninteractive
 
