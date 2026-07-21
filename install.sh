f1b3be244c7a0e96244bd888d7a45290b407db88
Peter Reuterås <peter@reuteras.net>
2026-07-21 07:19:11 +0200

    Update version of Malcolm

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 CONFIG_DIR="${HOME}/.config/malir"
 PATH="${PATH}:/usr/libexec/docker/cli-plugins"
-MALCOLM_VERSION="v26.06.1"
+MALCOLM_VERSION="v26.07.0"
 export PATH
 export DEBIAN_FRONTEND=noninteractive
 
