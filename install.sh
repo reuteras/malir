888837cd6af6a12e3c3dee21d6c4817fdcdd0f0a
Peter Reuterås <peter@reuteras.com>
2026-08-26 12:59:37 +0200

    Linter fixes and add MAXMIND_GEOIP_DB_ACCOUNT_ID

--- install.sh
+++ install.sh
@@ -15,55 +15,51 @@ function info-message() {
     echo "**** INFO: $*"
 }
 
-
 function error-message() {
     (>&2 echo "**** ERROR: $*")
 }
 
-
 function error-exit-message() {
     (>&2 echo "**** ERROR: $*")
-        exit 1
+    exit 1
 }
 
-
 # Function to update Ubuntu
-function update-os(){
+function update-os() {
     info-message "Running apt update."
     # shellcheck disable=SC2024
-    sudo apt update -qq > /dev/null 2>&1
+    sudo apt update -qq >/dev/null 2>&1
     info-message "Running apt dist-upgrade."
     # shellcheck disable=SC2024
-    while ! sudo DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade --force-yes > /dev/null 2>&1 ; do
+    while ! sudo DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade --force-yes >/dev/null 2>&1; do
         info-message "APT busy. Will retry in 10 seconds."
         sleep 10
     done
     info-message "Running apt install to install needed packages."
-    sudo DEBIAN_FRONTEND=noninteractive apt-get -y install apache2-utils ca-certificates curl moreutils openssl python3-ruamel.yaml python3-dotenv python3-dialog dialog> /dev/null 2>&1
-    if which snap > /dev/null ; then
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -y install apache2-utils ca-certificates curl moreutils openssl python3-ruamel.yaml python3-dotenv python3-dialog dialog >/dev/null 2>&1
+    if which snap >/dev/null; then
         info-message "Update snap."
         sudo snap refresh
     fi
     touch "${CONFIG_DIR}/os_done"
 }
 
-
 # Install Docker
-function install-docker(){
-    if dpkg --list | grep docker > /dev/null ; then
+function install-docker() {
+    if dpkg --list | grep docker >/dev/null; then
         touch "${CONFIG_DIR}/os_done"
     fi
-    sudo install -m 0755 -d /etc/apt/keyrings > /dev/null 2>&1
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc > /dev/null 2>&1
+    sudo install -m 0755 -d /etc/apt/keyrings >/dev/null 2>&1
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc >/dev/null 2>&1
     sudo chmod a+r /etc/apt/keyrings/docker.asc
     # shellcheck disable=SC1091
     echo \
-          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-            $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-              sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update > /dev/null 2>&1
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+            $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+        sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+    sudo apt-get update >/dev/null 2>&1
     info-message "Install Docker."
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null 2>&1
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >/dev/null 2>&1
     touch "${CONFIG_DIR}/docker_done"
 }
 
@@ -117,7 +113,6 @@ EOF
     exit
 }
 
-
 # Function to build Malcolm containers
 function malcolm-build() {
     info-message "Starting build process for docker containers."
@@ -142,42 +137,47 @@ function malcolm-build() {
     touch "${CONFIG_DIR}/build_done"
 }
 
-
 # Function to set MaxMind GeoIP license key for Arkime
 function malcolm-maxmind() {
     info-message "The build process needs your Maxmind API Key (free)"
     info-message "Get it from https://www.maxmind.com/"
     echo ""
     cd ~/Malcolm || exit
+    MAXMIND_ACCOUNT=""
     MAXMIND_KEY=""
+    while [[ -z "${MAXMIND_ACCOUNT}" ]]; do
+        read -rp "Maxmind GeoIP account ID: " MAXMIND_ACCOUNT
+    done
     while [[ -z "${MAXMIND_KEY}" ]]; do
         read -rp "Maxmind GeoIP license key (will echo key): " MAXMIND_KEY
     done
-    export MAXMIND_KEY
+    export MAXMIND_ACCOUNT MAXMIND_KEY
     echo ""
+    sed -i -e "s/MAXMIND_GEOIP_DB_ACCOUNT_ID=0/MAXMIND_GEOIP_DB_ACCOUNT_ID=$MAXMIND_ACCOUNT/" config/arkime-secret.env
     sed -i -e "s/MAXMIND_GEOIP_DB_LICENSE_KEY=0/MAXMIND_GEOIP_DB_LICENSE_KEY=$MAXMIND_KEY/" config/arkime-secret.env
-    if grep "MAXMIND_GEOIP_DB_LICENSE_KEY : '0'" config/arkime-secret.env > /dev/null 2>&1 ; then
+    if grep "MAXMIND_GEOIP_DB_ACCOUNT_ID=0" config/arkime-secret.env >/dev/null 2>&1; then
+        error-exit-message "Maxmind GeoIP License key not updated, exiting."
+    fi
+    if grep "MAXMIND_GEOIP_DB_LICENSE_KEY=0" config/arkime-secret.env >/dev/null 2>&1; then
         error-exit-message "Maxmind GeoIP License key not updated, exiting."
     fi
     touch "${CONFIG_DIR}/maxmind_done"
 }
 
-
 # Function to add intel from Critical Path Security to Zeek
-function malcolm-zeek-intel(){
+function malcolm-zeek-intel() {
     info-message "Clone Zeek intel from Critical Path Security"
     CDIR="$(pwd)"
     cd ~/Malcolm/zeek/intel || exit
-    git clone https://github.com/CriticalPathSecurity/Zeek-Intelligence-Feeds.git > /dev/null 2>&1
+    git clone https://github.com/CriticalPathSecurity/Zeek-Intelligence-Feeds.git >/dev/null 2>&1
     cd ~/Malcolm || exit
     sed -i -e "s_/usr/local/zeek/share/zeek/site/Zeek-Intelligence-Feeds_/opt/zeek/share/zeek/site/intel/Zeek-Intelligence-Feeds_" zeek/intel/Zeek-Intelligence-Feeds/main.zeek
     cd "${CDIR}" || exit
     touch "${CONFIG_DIR}/zeek_intel_done"
 }
 
-
 # Change nginx configuration - add nfa
-function nginx-configure(){
+function nginx-configure() {
     info-message "Configure nginx."
     cd ~/Malcolm || exit
     sed -i -e "/  upstream upload/i \ \ upstream nfa {\n    server nfa:5001;\n  }\n" nginx/nginx.conf
@@ -186,9 +186,8 @@ function nginx-configure(){
     touch "${CONFIG_DIR}/nginx_done"
 }
 
-
 # Function to change Arkime configuration
-function malcolm-configure-arkime(){
+function malcolm-configure-arkime() {
     info-message "Configure Arkime"
     cd ~/Malcolm || exit
     sed -i -e "s/magicMode=basic/magicMode=both/" arkime/etc/config.ini
@@ -206,13 +205,12 @@ function malcolm-configure-arkime(){
     touch "${CONFIG_DIR}/arkime_done"
 }
 
-
-function add-nfa(){
+function add-nfa() {
     info-message "Add nfa"
     cd ~/Malcolm || exit
-    [[ -d nfa ]] || git clone https://github.com/reuteras/nfa.git > /dev/null
+    [[ -d nfa ]] || git clone https://github.com/reuteras/nfa.git >/dev/null
     cp ~/malir/resources/nfa-config.ini nfa/config.ini
-    if ! grep "nfa:" docker-compose* > /dev/null 2>&1 ; then
+    if ! grep "nfa:" docker-compose* >/dev/null 2>&1; then
         sed -i "/services:/r ${HOME}/malir/resources/nfa-docker-compose.yml" docker-compose.yml
         sed -i "/services:/r ${HOME}/malir/resources/nfa-docker-compose-dev.yml" docker-compose-dev.yml
     fi
@@ -225,21 +223,19 @@ function add-nfa(){
 info-message "Start installation of Malcolm and extra tools."
 test -d "${CONFIG_DIR}" || mkdir -p "${CONFIG_DIR}"
 
-
 # Check for membership in group docker
-if ! grep "docker:" /etc/group | grep -E "(,|:)${USER}" > /dev/null; then
+if ! grep "docker:" /etc/group | grep -E "(,|:)${USER}" >/dev/null; then
     info-message "Add current user to group docker."
-    grep docker: /etc/group > /dev/null 2>&1 || sudo groupadd docker
+    grep docker: /etc/group >/dev/null 2>&1 || sudo groupadd docker
     sudo usermod -aG docker "$USER" || exit
     info-message "Logout and back in again to update group memberships."
     exit
 fi
 
-
 # Checkout Malcolm in home dir
 cd "${HOME}" || exit
-if !  test -d Malcolm ; then
-    git clone https://github.com/idaholab/Malcolm.git > /dev/null
+if ! test -d Malcolm; then
+    git clone https://github.com/idaholab/Malcolm.git >/dev/null
     cd Malcolm || exit
     git fetch --all --tags
     info-message "Using version $MALCOLM_VERSION of Malcolm."
